38de96430b3ee75d43a31e11b2e9aa759fa5acd4
Simon Collins <simon@devilish.io>
2016-02-22 18:38:07 +1100

    Implemented functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions allow you to create a wrapper
+## around R's standard matrix object and to use
+## this wrapper to cache the value of the matrix's inverse
+##
+## Example usage:
+##   myMatrix <- matrix(c(4,5,92,11,51,12,35,44,7),3,3)
+##   wrapped <- makeCacheMatrix(myMatrix)
+##   cacheSolve(wrapped)
+##   wrapped$set(myOtherMatrix)
 
-## Write a short comment describing this function
+## This function takes a R matrix object
+## and returns a wrapped object with methods to
+## allow the matrix's inverse to be cached for reuse
+## Parameters:
+##  currMatrix The matrix to initialise the wrapper with
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(currMatrix = matrix()) {
+  inv <- NULL
+  set <- function(newMatrix) {
+    # Update the current matrix
+    currMatrix <<- newMatrix
+    
+    # Clear cached inverse when a new matrix is set
+    inv <<- NULL
+  }
+  get <- function() currMatrix
+  setInverse <- function(i) inv <<- i
+  getInverse <- function() inv
+  
+  # Return our matrix wrapper object
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
+## This function takes a wrapped matrix object
+## created by makeCacheMatrix and solves it for its inverse
+## making use of the cached version if found.
+## Parameters:
+##   mx the matrix to solve for the inverse (assumed to be invertible)
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(mx) {
+  inv <- mx$getInverse()
+  # Check for cached inverse
+  if(!is.null(inv)) {
+    message("using precomputed inverse")
+    return(inv)
+  }
+  # not cached - compute, cache and return
+  data <- mx$get()
+  inv <- solve(data)
+  mx$setInverse(inv)
+  inv
 }
